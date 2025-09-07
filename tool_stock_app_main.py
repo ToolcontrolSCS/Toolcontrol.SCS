b4316479651ba413a78c1b42b4bee792a54f05c4
ToolcontrolSCS <toolcontrol.scs@gmail.com>
2025-09-07 19:59:04 +0700

    Update tool_stock_app_main.py

--- tool_stock_app_main.py
+++ tool_stock_app_main.py
@@ -117,7 +117,7 @@ tab_dash, tab_out, tab_in, tab_master, tab_txn = st.tabs(
 # Dashboard
 # -------------------------------
 with tab_dash:
-    st.markdown("## 📊 Stock Health Overview")
+    st.markdown("## 📊 Tool Stock Overview")
 
     try:
         df_bal = pd.DataFrame(
@@ -325,3 +325,4 @@ with tab_txn:
 
         st.dataframe(styled_txn, use_container_width=True)
         st.download_button("⬇️ Export Transactions CSV", data=dft.to_csv(index=False), file_name="transactions_export.csv")
+
